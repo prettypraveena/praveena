460e8ac809c92b9e8546a83b9846d36cb9c18630
prettypraveena <57347278+prettypraveena@users.noreply.github.com>
2020-01-22 16:32:14 +0530

    Add files via upload

---
+++ One2Many/src/one2M/Question.java
@@ -0,0 +1,71 @@
+package one2M;
+
+import javax.persistence.*;  
+
+import java.util.List;    
+
+  
+
+@Entity  
+
+@Table(name="query")  
+
+public class Question {    
+
+  
+
+@Id   
+
+@GeneratedValue(strategy=GenerationType.TABLE)  
+
+private int id;    
+
+private String qname;    
+
+  
+
+@OneToMany(cascade = CascadeType.ALL)  
+
+@JoinColumn(name="qid")  
+
+@OrderColumn(name="type")  
+
+private List<Answer> answers;  
+
+public int getId() {  
+
+    return id;  
+
+}  
+
+public void setId(int id) {  
+
+    this.id = id;  
+
+}  
+
+public String getQname() {  
+
+    return qname;  
+
+}  
+
+public void setQname(String qname) {  
+
+    this.qname = qname;  
+
+}  
+
+public List<Answer> getAnswers() {  
+
+    return answers;  
+
+}  
+
+public void setAnswers(List<Answer> answers) {  
+
+    this.answers = answers;  
+
+}      
+
+}  
